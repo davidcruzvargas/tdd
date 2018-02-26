bb6d2d1f0c13c6ec4905e0c5f3753eef84f3763e
DavidCruz <dcruz@wfscorp.com>
2018-02-25 21:31:21 -0500

    Step 3. REFACTOR, refactor of the test and javadoc included. The code is cleaner and the test still running.

--- src/main/java/com/football/tdd/FootballTeam.java
+++ src/main/java/com/football/tdd/FootballTeam.java
@@ -1,10 +1,12 @@
 package com.football.tdd;
 
+/**
+ * @author DACV
+ * 
+ *         This class represents a football team
+ */
 public class FootballTeam {
 
-	// To fix the code this variable was added and the constructor and the getter
-	// now uses this variable to store games won. This is the minimum code to pass
-	// the test.
 	private int gamesWon;
 
 	public FootballTeam(int gamesWon) {
--- src/test/java/com/football/tdd/FootballTeamTest.java
+++ src/test/java/com/football/tdd/FootballTeamTest.java
@@ -4,17 +4,28 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+/**
+ * @author DACV
+ * 
+ *         This class contains all the tests that are applicable to a
+ *         {@link FootballTeam} class.
+ */
 public class FootballTeamTest {
 
+	private static final int THREE_GAMES_WON = 3;
+
+	// If you have a self-explained name for your method, then, is not necessary
+	// include a Javadoc.
 	@Test
 	public void constructorShouldSetGamesWon() {
-		FootballTeam team = new FootballTeam(3);
 
-		// Remember always improve your error messages until the message be clear enough
-		// to detect the error.
-		assertEquals("3 games passed to constructor, but " + team.getGamesWon() + " were returned.", 3,
-				team.getGamesWon());
+		// In this step (REFACTOR) is really important refactor all the code, even the
+		// tests code. Now the test is not using the magic number all over the place, is
+		// using the centralized constant of THREE_GAMES_WON
 
-		// This test is passing, and, in this step (GREEN), is a good thing.
+		FootballTeam team = new FootballTeam(THREE_GAMES_WON);
+
+		assertEquals(THREE_GAMES_WON + " games passed to constructor, but " + team.getGamesWon() + " were returned.",
+				THREE_GAMES_WON, team.getGamesWon());
 	}
 }
