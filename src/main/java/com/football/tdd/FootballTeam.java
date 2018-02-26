a50cf37da11414963db5e3714ac2d2b8f94316cc
DavidCruz <dcruz@wfscorp.com>
2018-02-25 20:59:17 -0500

    Step 1. RED, write the first test and verify is failing. The code compiles but the test fails.

---
+++ src/main/java/com/football/tdd/FootballTeam.java
@@ -0,0 +1,18 @@
+package com.football.tdd;
+
+public class FootballTeam {
+
+	public FootballTeam(int gamesWon) {
+		// TODO Auto-generated constructor stub
+		// At this point there is no code here, is the minimum code to compile the
+		// project and execute the test.
+	}
+
+	public int getGamesWon() {
+		// TODO Auto-generated method stub
+		// At this point there is no code here, is the minimum code to compile the
+		// project and execute the test.
+		return 0;
+	}
+
+}
---
+++ src/test/java/com/football/tdd/FootballTeamTest.java
@@ -0,0 +1,21 @@
+package com.football.tdd;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FootballTeamTest {
+
+	@Test
+	public void constructorShouldSetGamesWon() {
+		FootballTeam team = new FootballTeam(3);
+
+		// Remember always improve your error messages until the message be clear enough
+		// to detect the error.
+		assertEquals("3 games passed to constructor, but " + team.getGamesWon() + " were returned.", 3,
+				team.getGamesWon());
+		
+		// This test is failing, and, in this step (RED - white a failing test), is a
+		// good thing.
+	}
+}
